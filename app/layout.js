999bba865939d0573115bcb971fccb48a508b579
faisalfz <faisalfiaz40@gmail.com>
2023-05-15 23:32:01 +0500

    Initial commit, install dependencies

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Provider from '@/components/Provider'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Provider>
+      {children}
+      </Provider>
+      </body>
     </html>
   )
 }
